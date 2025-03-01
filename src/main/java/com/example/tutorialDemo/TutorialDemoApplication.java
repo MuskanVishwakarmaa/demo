ccba6c044a2e812e9b7e38087f35bdd3d9f85982
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-01 14:43:49 +0530

    FIRST

---
+++ src/main/java/com/example/tutorialDemo/TutorialDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDemoApplication.class, args);
+	}
+
+}
